f26d19babe7073cef86722ae947f98f146ca6c13
vzakharchenko <v.zakharchenko@tempo.io>
2025-03-05 08:23:17 +0200

    fixed build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,29 +1,33 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import path from "node:path";
 
-export default defineConfig({
-    ssr: {
-        external:  ['@forge/sql'],
-        optimizeDeps: true,
-        emitAssets: false,
-    },
-    build: {
-        lib: {
-            entry: path.resolve("src/index.ts"),
-            name: "ForgeSQLORM",
-            formats: ["cjs", "es"],
-            fileName: (format) => `forge-sql-orm.${format}.js`,
+export default defineConfig(({ mode }) => {
+    const isCLI = mode === "cli"; // Передавать `--mode cli` при сборке CLI
+    return {
+        ssr: {
+            external: ["@forge/sql"],
+            optimizeDeps: true,
+            emitAssets: false,
         },
-        ssr: true,
-        sourcemap: true,
-        target: "node18",
-        rollupOptions: {
-            external: ['@forge/sql'],
-            output: {
-                esModule: true,
-                externalImportAttributes: true,
-                exports: "named",
-            }
+        build: {
+            outDir: isCLI ? "dist-cli" : "dist", // Разделяем выходные папки
+            lib: {
+                entry: isCLI
+                    ? { cli: path.resolve("scripts/cli.ts") }
+                    : { ForgeSQLORM: path.resolve("src/index.ts") },
+                formats: ["cjs", "es"],
+            },
+            ssr: true,
+            sourcemap: true,
+            target: "node18",
+            rollupOptions: {
+                external: ["@forge/sql"],
+                output: {
+                    esModule: true,
+                    externalImportAttributes: true,
+                    exports: "named",
+                },
+            },
         },
-    },
+    };
 });
