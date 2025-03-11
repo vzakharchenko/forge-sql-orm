6f324674d2e8cfa0aad431389b43c8e98a6ae357
vzakharchenko <v.zakharchenko@tempo.io>
2025-03-11 22:26:19 +0200

    fix security issue

--- scripts/forgeSqlOrmCLI.js
+++ scripts/forgeSqlOrmCLI.js
@@ -10,15 +10,10 @@ const args = process.argv.slice(2).join(" ");
 // Resolve the path to cli.ts (your TypeScript entry file)
 const cliPath = path.resolve(__dirname, "cli.js");
 
-// Function to run a command
-const runCommand = (cmd) => {
-  try {
-    execSync(cmd, { stdio: "inherit", shell: true });
-    process.exit(0);
-  } catch (e) {
-    console.error("⚠️  Command execution failed:", e.message);
-    process.exit(1);
-  }
-};
-
-runCommand(`npx  --yes tsm --no-warnings ${cliPath} ${args}`);
+try {
+  execSync(`npx  --yes tsm --no-warnings ${cliPath} ${args}`, { stdio: "inherit", shell: true });
+  process.exit(0);
+} catch (e) {
+  console.error("⚠️  Command execution failed:", e.message);
+  process.exit(1);
+}
