114bae796ad2c77639f79ef638909fda5d829f8e
vzakharchenko <v.zakharchenko@tempo.io>
2025-04-08 21:56:50 +0300

    added coverall

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+
     globals: true,
     mockReset: true,
     coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
       include: ["src"],
+      reportsDirectory: './coverage',
     },
     include: ['src/**/*.test.ts', '__tests__/**/*.test.ts'],
   },
