bbcf20c2322b7ddfd43b45cc7a23e4c1d7411f94
vzakharchenko <v.zakharchenko@tempo.io>
2025-04-29 19:33:54 +0300

    update dependencies

--- autupdate.sh
+++ autupdate.sh
@@ -15,9 +15,11 @@ ncu -u
 npm i
 cd static/forge-orm-example
 ncu -u
+rm -rf package-lock.json
 npm i
 npm run build
 sleep 2s
+git add -f package-lock.json
 git add -f build-static/*
 cd ../../..
 
@@ -26,9 +28,11 @@ ncu -u
 npm i
 cd static/forge-orm-example
 ncu -u
+rm -rf package-lock.json
 npm i
 npm run build
 sleep 2s
+git add -f package-lock.json
 git add -f build-static/*
 cd ../../..
 
@@ -37,9 +41,11 @@ ncu -u
 npm i
 cd static/forge-orm-example
 ncu -u
+rm -rf package-lock.json
 npm i
 npm run build
 sleep 2s
+git add -f package-lock.json
 git add -f build-static/*
 cd ../../..
 
@@ -48,9 +54,11 @@ ncu -u
 npm i
 cd static/forge-orm-example
 ncu -u
+rm -rf package-lock.json
 npm i
 npm run build
 sleep 2s
+git add -f package-lock.json
 git add -f build-analize/*
 cd ../../..
 
@@ -59,8 +67,10 @@ ncu -u
 npm i
 cd static/forge-orm-example
 ncu -u
+rm -rf package-lock.json
 npm i
 npm run build
-sleep 5s
+sleep 2s
+git add -f package-lock.json
 git add -f build-static/*
 cd ../../..
