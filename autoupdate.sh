28a51383be7bb2cfae7e3a74e40042b7f9586c36
vzakharchenko <vaszakharchenko@gmail.com>
2025-06-29 04:57:15 +0300

    update dependencies

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 npm run build
 cd forge-sql-orm-cli
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 npm run build
 cd ../examples
 
 cd forge-sql-orm-example-drizzle-driver-simple
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -27,11 +27,11 @@ cd ../../..
 
 cd forge-sql-orm-example-dynamic
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -42,11 +42,11 @@ cd ../../..
 
 cd forge-sql-orm-example-optimistic-locking
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -57,11 +57,11 @@ cd ../../..
 
 cd forge-sql-orm-example-query-analyses
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -72,11 +72,11 @@ cd ../../..
 
 cd forge-sql-orm-example-simple
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -87,11 +87,11 @@ cd ../../..
 
 cd forge-sql-orm-example-checklist
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -102,11 +102,11 @@ cd ../../..
 
 cd forge-sql-orm-example-org-tracker
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
@@ -117,11 +117,11 @@ cd ../../..
 
 cd forge-sql-orm-example-sql-executor
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 npm i
 cd static/forge-orm-example
 rm -rf node_modules
-ncu -u
+ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
