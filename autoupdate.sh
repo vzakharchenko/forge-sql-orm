34123ba2ec85b8aa7e2fd5250170d9af10c3f723
vzakharchenko <mria-v@mriacorp.com>
2025-08-05 14:06:26 +0300

    security fix

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,156 +2,158 @@
 set -e
 
 ncu -u --dep prod,dev,peer
+rm -rf node_modules package-lock.json
 npm i
 npm run build
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd forge-sql-orm-cli
 ncu -u --dep prod,dev,peer
+rm -rf node_modules package-lock.json
 npm i
 npm run build
 cd ../examples
 
 cd forge-sql-orm-example-drizzle-driver-simple
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
 
 cd forge-sql-orm-example-dynamic
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
 
 cd forge-sql-orm-example-optimistic-locking
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
 
 cd forge-sql-orm-example-query-analyses
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-analize/*
 cd ../../..
 
 cd forge-sql-orm-example-simple
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
 
 cd forge-sql-orm-example-checklist
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-article/*
 cd ../../..
 
 cd forge-sql-orm-example-org-tracker
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
 
 cd forge-sql-orm-example-sql-executor
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 npm i
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 cd static/forge-orm-example
-rm -rf node_modules
+rm -rf node_modules package-lock.json
 ncu -u --dep prod,dev,peer
 rm -rf package-lock.json
 npm i
 npm run build
 sleep 2
-npm add -f package.json
-npm add -f package-lock.json
+git add package.json
+git add package-lock.json
 git add -f build-static/*
 cd ../../..
