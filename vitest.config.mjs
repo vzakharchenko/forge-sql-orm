aa72b378e5f72263150ce48a422c0cb3a0057fdc
vzakharchenko <vaszakharchenko@gmail.com>
2025-11-28 09:28:30 +0200

    added test thresholds

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -10,6 +10,12 @@ export default defineConfig({
       reporter: ['text', 'lcov'],
       include: ["src"],
       reportsDirectory: './coverage',
+        thresholds: {
+            statements: 80,
+            branches: 75,
+            functions: 80,
+            lines: 80,
+        },
     },
     include: ['src/**/*.test.ts', '__tests__/**/*.test.ts'],
   },
